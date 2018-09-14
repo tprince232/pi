49d19433c9ae2eebbf5ab26de9e749cdb7ff7aca
tprince232 <troyprince@outlook.com>
2018-09-14 04:09:26 +0000

    initial commit

---
+++ home.js
@@ -0,0 +1,28 @@
+function loadJSON(callback) {
+    console.log("in load");
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'users.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+	if (xobj.readyState == 4 && xobj.status == "200") {
+	    // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+	    callback(xobj.responseText);
+	}
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function(response) {
+	// Parse JSON string into object
+	var teams = JSON.parse(response);
+	for(var team in teams) {
+	    var para = document.createElement("p");
+	    var node = document.createTextNode(team.p1);
+	    para.appendChild(node);
+	    var element = document.getElementById("main");
+	    element.appendChild(para);
+    });
+}
+
+init()
